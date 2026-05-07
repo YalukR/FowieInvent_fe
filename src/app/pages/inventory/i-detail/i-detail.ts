b0b8b750bde29d46aa3d41e5017f9be8d34d25bf
YalukR <yalukramos@gmail.com>
2026-05-06 21:44:09 -0600

    componentes creados

---
+++ src/app/pages/inventory/i-detail/i-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-i-detail",
+  imports: [],
+  templateUrl: "./i-detail.html",
+  styleUrl: "./i-detail.scss",
+})
+export class IDetail {}
---
+++ src/app/pages/inventory/i-modal/i-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-i-modal",
+  imports: [],
+  templateUrl: "./i-modal.html",
+  styleUrl: "./i-modal.scss",
+})
+export class IModal {}

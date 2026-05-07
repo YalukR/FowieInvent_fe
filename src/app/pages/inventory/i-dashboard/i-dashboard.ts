e811fed7e4864665faaf99ae4b3a8ddc59463231
YalukR <yalukramos@gmail.com>
2026-05-06 18:30:24 -0600

    cambios incompletos

--- src/app/pages/inventory/i-dashboard/i-dashboard.ts
+++ src/app/pages/inventory/i-dashboard/i-dashboard.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import { TableModule } from "primeng/table";
 
 @Component({
   selector: "app-i-dashboard",
-  imports: [],
+  imports: [
+    TableModule
+  ],
   templateUrl: "./i-dashboard.html",
   styleUrl: "./i-dashboard.scss",
 })
-export class IDashboard {}
+export class IDashboard {
+  
+}

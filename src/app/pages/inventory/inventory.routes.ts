e1f87500397d161a0b6119cb94812374a74b7217
YalukR <yalukramos@gmail.com>
2026-05-06 21:49:39 -0600

    rutas de detalle y modal

--- src/app/pages/inventory/inventory.routes.ts
+++ src/app/pages/inventory/inventory.routes.ts
@@ -9,6 +9,8 @@ export default [
         children: [
             { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
             { path: 'dashboard', loadComponent: () => import('./i-dashboard/i-dashboard').then(m => m.IDashboard) },
+            { path: 'detail', loadComponent: () => import('./i-detail/i-detail').then(m => m.IDetail) },
+            { path: 'modal', loadComponent: () => import('./i-modal/i-modal').then(m => m.IModal) },
         ]
     },
 ] as Routes;

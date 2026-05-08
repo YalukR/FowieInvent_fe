4e0aab0e7a339b6ce6e154b991ca7fa079afabae
YalukR <yalukramos@gmail.com>
2026-05-08 17:58:26 -0600

    rutas actualizadas

--- src/app/pages/inventory/inventory.routes.ts
+++ src/app/pages/inventory/inventory.routes.ts
@@ -9,8 +9,9 @@ export default [
             { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
             { path: 'dashboard', loadComponent: () => import('./i-dashboard/i-dashboard').then(m => m.IDashboard) },
             { path: 'products', loadComponent: () => import('./i-products/i-products').then(m => m.IProducts) },
-            { path: 'detail', loadComponent: () => import('./i-detail/i-detail').then(m => m.IDetail) },
-            { path: 'modal', loadComponent: () => import('./i-pmodal/i-modal').then(m => m.IModal) },
+            { path: 'products/:id', loadComponent: () => import('./i-products/i-pdetail/i-pdetail').then(m => m.IPdetail) },
+            { path: 'categories', loadComponent: () => import('./i-categories/i-categories').then(m => m.ICategories) },
+            { path: 'modal', loadComponent: () => import('./i-products/i-pmodal/i-modal').then(m => m.IModal) },
         ]
     },
 ] as Routes;

3de3125369f6b82138425873e55fef8b0469369f
YalukR <yalukramos@gmail.com>
2026-05-08 18:39:04 -0600

    categories detail

--- src/app/pages/inventory/inventory.routes.ts
+++ src/app/pages/inventory/inventory.routes.ts
@@ -11,6 +11,7 @@ export default [
             { path: 'products', loadComponent: () => import('./i-products/i-products').then(m => m.IProducts) },
             { path: 'products/:id', loadComponent: () => import('./i-products/i-pdetail/i-pdetail').then(m => m.IPdetail) },
             { path: 'categories', loadComponent: () => import('./i-categories/i-categories').then(m => m.ICategories) },
+            { path: 'categories/:id', loadComponent: () => import('./i-categories/i-cdetail/i-cdetail').then(m => m.ICdetail) },
             { path: 'modal', loadComponent: () => import('./i-products/i-pmodal/i-modal').then(m => m.IModal) },
         ]
     },

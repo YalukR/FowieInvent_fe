751275bdbcc84ec53b6c0fc8ddbd5329a6d9f8e1
YalukR <yalukramos@gmail.com>
2026-05-10 12:36:18 -0600

    componente de movimientos

--- src/app/pages/inventory/i-products/i-pmovements/i-pmovements.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-i-pmovements",
-  imports: [],
-  templateUrl: "./i-pmovements.html",
-  styleUrl: "./i-pmovements.scss",
-})
-export class IPmovements {}

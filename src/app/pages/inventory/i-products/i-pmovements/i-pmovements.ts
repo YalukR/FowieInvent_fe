b65b28db7e4842b53f32de1c2cdce8f8bbaf12c1
YalukR <yalukramos@gmail.com>
2026-05-09 21:19:22 -0600

    modulo de movimientos

---
+++ src/app/pages/inventory/i-products/i-pmovements/i-pmovements.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-i-pmovements",
+  imports: [],
+  templateUrl: "./i-pmovements.html",
+  styleUrl: "./i-pmovements.scss",
+})
+export class IPmovements {}

b052fef575028669a9cacea6966644b856af8fd8
YalukR <yalukramos@gmail.com>
2026-05-11 15:34:02 -0600

    permisos cargados desde el back

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { AuthService } from './app/core/service/auth.service';
+import { inject } from '@angular/core';
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [RouterModule],
     template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {}
+export class AppComponent {
+    authService = inject(AuthService)
+
+    ngOnInit() {
+        if (this.authService.isLoggedIn()) {
+            this.authService.fetchMe().subscribe();
+        }
+    }
+
+}

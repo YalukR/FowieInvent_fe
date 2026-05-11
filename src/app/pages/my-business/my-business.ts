7518ee27e7b3748922a5882779f2f46653d420a0
YalukR <yalukramos@gmail.com>
2026-05-10 20:56:09 -0600

    modulos del sistema

---
+++ src/app/pages/my-business/my-business.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-my-business",
+  imports: [],
+  templateUrl: "./my-business.html",
+  styleUrl: "./my-business.scss",
+})
+export class MyBusiness {}
---
+++ src/app/pages/rbac/rbac.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-rbac",
+  imports: [],
+  templateUrl: "./rbac.html",
+  styleUrl: "./rbac.scss",
+})
+export class Rbac {}

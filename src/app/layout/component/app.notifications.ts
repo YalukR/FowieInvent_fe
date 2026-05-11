64d413f56e0dc257019775cfd992993cd51d9965
YalukR <yalukramos@gmail.com>
2026-05-10 21:48:11 -0600

    notificaciones component

---
+++ src/app/layout/component/app.notifications.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+    selector: 'app-notifications',
+    standalone: true,
+    imports: [
+        CommonModule
+    ],
+    template: ``,
+})
+
+export class AppNotifications {
+
+}

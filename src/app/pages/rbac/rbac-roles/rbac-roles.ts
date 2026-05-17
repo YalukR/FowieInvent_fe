872c6f46484bac422b340822b49018ffc3429c4e
YalukR <yalukramos@gmail.com>
2026-05-16 18:10:34 -0600

    nuevos modulos

---
+++ src/app/pages/rbac/rbac-roles/rbac-roles.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-rbac-roles",
+  imports: [],
+  templateUrl: "./rbac-roles.html",
+  styleUrl: "./rbac-roles.scss",
+})
+export class RbacRoles {}
